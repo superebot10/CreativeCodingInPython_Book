4890e86775ff2b8b330ca7aefc53d6f0e4cc9da2
Eric Carver <ecarver@localhost.localdomain>
2020-04-13 12:40:15 -0400

    first commit

---
+++ Lv1_Chatbot.py
@@ -0,0 +1,12 @@
+print('Hello. I am Explody. I am a chatbot.')
+print('I like animals and I love to talk about food.')
+name = input('What is your name?: ')
+print('Hello', name, '. Nice to meet you.')
+year = input('I am not very good with dates. What is the year?: ')
+print('Yes I think that is correct. Thanks!')
+myage = input('Can you guess my age? -enter a number: ')
+print('Yes you are right. I am ', myage)
+myage = int(myage)
+nyears = 100 - myage
+print('I will be 100 in ',nyears, ' years.')
+print('That will be in the year ', int(year) + nyears)
