c943b6f5bc0d597d6c2736ce00dc731d5dd297c8
Eric Carver <ecarver@localhost.localdomain>
2020-04-13 12:59:54 -0400

    go

---
+++ .vscode/Findedges.py
@@ -0,0 +1,17 @@
+import turtle
+import time
+shelly = turtle.Turtle()
+
+shelly.left(90)
+shelly.forward(400)
+
+
+
+# X corrdinates are 715 and -715
+# Y corrdinates are 400 and -400
+
+
+while True:
+    a = input('Press "e" to exit.')
+    if a == 'e'.lower():
+        break
