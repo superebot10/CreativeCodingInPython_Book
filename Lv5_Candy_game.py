1808be70bd2a170847b13a1471bed1af1a934bc3
Chad Carver <ccarver@localhost.localdomain>
2020-04-18 20:07:55 -0600

    Final changes

--- Lv5_Candy_game.py
+++ Lv5_Candy_game.py
@@ -7,7 +7,8 @@ window = Tk()
 window.title('The Yummyness Game')
 
 # create a canvas to put objects on the screen
-canvas = Canvas(window, width=1000, height=500, bg = 'black')
+#canvas = Canvas(window, width=1000, height=500, bg = 'black')
+canvas = Canvas(window, width=400, height=400, bg = 'black')
 canvas.pack()
 
 # set up welcome screen with title and directions
@@ -27,7 +28,8 @@ level_display.pack()
 # create an image object using the gif file
 player_image = PhotoImage(file="greenChar.gif")
 # use image object to create a character at position 500, 480
-mychar = canvas.create_image(500, 480, image = player_image)
+#mychar = canvas.create_image(500, 480, image = player_image)
+mychar = canvas.create_image(200, 360, image = player_image)
 
 # variables and lists needed for managing candy
 candy_list = [] # list containing all candy created, empty at start
@@ -87,4 +89,67 @@ def end_game_over():
 def end_title():
     canvas.delete(title)       #remove title
     canvas.delete(directions)  #remove directions
+
+# check distance between 2 objects - return true if they 'touch'
+def collision(item1, item2, distance):
+    xdistance = abs(canvas.coords(item1)[0] - canvas.coords(item2)[0])
+    ydistance = abs(canvas.coords(item1)[1] - canvas.coords(item2)[1])
+    overlap = xdistance < distance and ydistance < distance
+    return overlap
+
+# checks if character hits bad candy, schedule end_game_over
+# if character hits candy, remove from screen, list, update score
+def check_hits():
+    # check if it hit a bad candy - need to end game
+    for candy in bad_candy_list:
+        if collision(mychar, candy, 30):
+            game_over = canvas.create_text(200, 200, text= 'Game Over :(', fill= 'red', font = ('Helvetica', 30))
+            # end game but after user can see score
+            window.after(4000, end_game_over)
+            # do not check any other candy, window to be destroyed
+            return
+    # check if hit any good candy
+    for candy in candy_list:
+        if collision(mychar, candy, 30):
+            canvas.delete(candy) # remove from canvas
+            # find where in list and remove a nd update score
+            candy_list.remove(candy)
+            update_sore_level()
+        # schedule check hits again
+    window.after(100, check_hits)
+
+move_direction = 0 # track which direction player is moving
+# function handles when user first presses arrow keys
+def check_input(event):
+    global move_direction
+    key = event.keysym
+    if key == "Right":
+        move_direction = "Right"
+    elif key == "Left":
+        move_direction = "Left"
+
+# Function handles when user stops pressing arrow key
+def end_input(event):
+    global move_direction
+    move_direction = "None"
+
+# Function checks if not on edge and updates x coordinates based on right/left
+def move_character():
+    if move_direction == "Right" and canvas.coords(mychar)[0] < 400:
+        canvas.move(mychar, 10,0)
+    if move_direction == "Left" and canvas.coords(mychar)[0] > 0:
+        canvas.move(mychar, -10,0)
+    window.after(16, move_character) # Move character at 60 frames per second
+
+# bind the keys to the character
+canvas.bind_all('<KeyPress>', check_input) # Bind key input
+canvas.bind_all('<KeyRelease>', end_input) # bind all keys to circle
+
+# Start game loop by scheduling all the functions
+window.after(1000, end_title) # destroy title and instructions
+window.after(1000, make_candy) # start making candy
+window.after(1000, move_candy) # start moving candy
+window.after(1000, check_hits) # check if character hit a candy
+window.after(1000, move_character) # handle keyboard controls
+
 window.mainloop()
